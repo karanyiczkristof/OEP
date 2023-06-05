ba869cf2556379c82b07bcb023406f817ffc1db7
karanyiczkristof <135637639+karanyiczkristof@users.noreply.github.com>
2023-06-05 18:05:52 +0200

    Add files via upload

---
+++ HarmadikBead/HarmadikBead/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace HarmadikBead
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   
+
+            
+            File Kepek = new File(33);
+            File OEP = new File(16);
+
+
+            Console.WriteLine("Ennyi memóriát foglal a Képek mappa: {0}", Kepek.GetSize());
+            
+        }
+    }
+}
---
+++ HarmadikBead/HarmadikBead/Registration.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HarmadikBead
+{
+    abstract class Registration
+    {
+
+        public abstract int GetSize(); 
+       
+        
+    }
+
+
+    class File : Registration
+    {
+        private int size;
+
+        public File(int s) 
+        {
+            size = s;
+        }
+        public override int GetSize()
+        {
+            return size;
+        }
+    }
+
+    class Folder : Registration
+    {
+
+        private List<Registration> items = new List<Registration>();
+        public override int GetSize()
+        {
+            int sum = 0;
+
+            foreach (Registration b in items)
+            {
+                sum += b.GetSize();
+            }
+            return sum;
+        }
+
+        public void Add(Registration r)
+        {
+            items.Add(r);
+        }
+    }
+
+    class FileSystem : Folder
+    {
+        
+    }
+
+}
